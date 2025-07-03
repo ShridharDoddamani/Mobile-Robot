03ed7b022d02cf3e5e23d6c76ed81bf8b56f93d1
ShridharDoddamani <shridharhi99@gmail.com>
2025-07-03 17:51:59 +0530

    Add files via upload

---
+++ Simulation in Webots/controllers/obstacleavd/build/release/obstacleavd.d
@@ -0,0 +1,5 @@
+build/release/obstacleavd.o: obstacleavd.cpp \
+ C:\Users\HP\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Users\HP\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Users\HP\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Users/HP/AppData/Local/Programs/Webots/include/controller/c/webots/types.h
